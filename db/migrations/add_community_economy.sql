38b65868c170a3446f926e8d5b3bca51b88a00f9
eksucampusmarketplace-cell <eksucampusmarketplace@gmail.com>
2026-04-04 15:52:18 -0700

    Merge pull request #259 from eksucampusmarketplace-cell/cto/community-credit-systemusers-build-a-telegram-reputation-sco
    Engine: Task changes

--- db/migrations/add_community_economy.sql
+++ db/migrations/add_community_economy.sql
@@ -85,6 +85,7 @@ CREATE TABLE IF NOT EXISTS community_quests (
 
 -- User quest progress
 CREATE TABLE IF NOT EXISTS community_quest_progress (
+    id BIGSERIAL PRIMARY KEY,
     quest_id BIGINT REFERENCES community_quests(id),
     user_id BIGINT NOT NULL,
     progress_value INTEGER DEFAULT 0,
